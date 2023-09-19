0956b4c0d5d60145714d14d6dc53b252eab38b47
adibisoyi <abisoyi@uccs.edu>
2023-09-19 15:40:43 -0600

    Reset Secret key

--- django_project/settings.py
+++ django_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ytp6=j($5np#9&%v0^!&_kf(s8w^6s=-y@ok!@&tl_@3h0)onc'
+SECRET_KEY = 'django-insecure-i@0d@_%0e25p!-mc9uh*e_k%7p4@)8ra74kbm$cgs8wjl7b4z('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
