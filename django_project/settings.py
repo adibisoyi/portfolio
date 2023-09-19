ab99fb8e28499778740c14e89a56aa5ce536d3b7
adibisoyi <abisoyi@uccs.edu>
2023-09-19 15:31:11 -0600

    Updated settings file

--- django_project/settings.py
+++ django_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i@0d@_%0e25p!-mc9uh*e_k%7p4@)8ra74kbm$cgs8wjl7b4z('
+SECRET_KEY = 'django-insecure-ytp6=j($5np#9&%v0^!&_kf(s8w^6s=-y@ok!@&tl_@3h0)onc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #adding the app name
+    'portfolio_app',
+]
+#add support for authenticating users
+AUTHENTICATION_BACKEND = [
+    'django.contrib.auth.backends.ModelBackend',
 ]
 
 MIDDLEWARE = [
