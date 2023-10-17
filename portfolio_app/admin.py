855e4ee9fa770dfe15c3d375032fdc77aafbed83
adibisoyi <abisoyi@uccs.edu>
2023-10-17 10:55:47 -0600

    GE03

--- portfolio_app/admin.py
+++ portfolio_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Student
+from .models import Portfolio
+from .models import Project
+from .models import ProjectsInPortfolio
+
+# Register your models here so they can be edited in admin panel
+admin.site.register(Student)
+admin.site.register(Portfolio)
+admin.site.register(Project)
+admin.site.register(ProjectsInPortfolio)
+
